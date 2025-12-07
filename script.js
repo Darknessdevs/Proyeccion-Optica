5d22b1db591b68cc917a62550064627b4a54edee
Darknessdevs <santicraftello@gmail.com>
2025-12-07 18:01:29 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const observerOptions = {
+    threshold: 0.15,
+    rootMargin: '0px 0px -80px 0px'
+};
+
+const observer = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.service-card').forEach(card => {
+    observer.observe(card);
+});
